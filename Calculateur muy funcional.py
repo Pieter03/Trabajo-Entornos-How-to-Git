58a202b978a45c5f09f274a4ee85019c552117b3
Pedro Borregan Burgos <2021442@iesluisbraille.es>
2022-01-27 09:41:40 +0100

    V1.0

---
+++ Calculateur muy funcional.py
@@ -0,0 +1,17 @@
+import math
+
+"""Calculadora"""
+print("Calculdora de dos números")
+print("                         ")
+print("1. Suma")
+print("2. Resta")
+print("3. Multiplifación")
+print("4. División")
+print("5. Cociente")
+print("6. Resto")
+print("7. Exponenciación")
+print("8. Área Cuadrado")
+print("9. Área Rectángulo")
+print("10. Área Triángulo")
+print("11. Área Círculo")
+print("12. Salir")
