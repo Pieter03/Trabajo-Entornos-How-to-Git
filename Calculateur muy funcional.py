c1a42f9dd24299721e35b80de98af05dac7b59f4
Pedro Borregan Burgos <2021442@iesluisbraille.es>
2022-01-27 09:58:30 +0100

    V3.0
    (More simple operations added. Functional, but still low functionality).

--- Calculateur muy funcional.py
+++ Calculateur muy funcional.py
@@ -15,3 +15,23 @@ print("9. Área Rectángulo")
 print("10. Área Triángulo")
 print("11. Área Círculo")
 print("12. Salir")
+
+opcion=int(input("Seleccione el tipo de operación según su número asignado: "))
+
+while opcion<1 or opcion>12: #Con este controlamos que este en el rango correcto
+    opcion=int(input("Introduce le numero valido"))
+#Suma
+if(opcion==1):
+    resSuma= lambda x, y: x+y
+    numero1=int(input("Introduce el primer número: "))
+    numero2=int(input("Introduce el segundo número: "))
+    print(f"{numero1} + {numero2} = {resSuma(numero1, numero2)}")
+
+
+#Resta    
+elif(opcion==2):
+    resResta= lambda x, y: x-y
+    numero1=int(input("Introduce el primer número: "))
+    numero2=int(input("Introduce el segundo número: "))
+    resultado1=(numero1 - numero2)
+    print(f"{numero1} - {numero2} = {resResta(numero1, numero2)}")
