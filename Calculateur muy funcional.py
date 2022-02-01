906bb6719447a1426836ace47c6ed6125247a098
Pedro Borregan Burgos <2021442@iesluisbraille.es>
2022-02-01 12:52:56 +0100

    V6.0
    (Union and adding exit option)

--- Calculateur muy funcional.py
+++ Calculateur muy funcional.py
@@ -54,7 +54,36 @@ elif(opcion==4):
     else:
         resultado1=(numero1 / numero2)
         print(f"{numero1} / {numero2} = {resDivision(numero1, numero2)}")
-        
+
+#Cociente
+elif(opcion==5):
+    resCociente= lambda x, y: x//y
+    numero1=int(input("Introduce el primer número: "))
+    numero2=int(input("Introduce el segundo número: "))
+    if(numero2==0):
+        print("No es posible dividir entre 0")
+    else:
+        resultado1=(numero1 // numero2)
+        print(f"Cociente de {numero1} // {numero2} = {resCociente(numero1, numero2)}")
+
+#Resto   
+elif(opcion==6):
+    resResto= lambda x, y: x%y
+    numero1=int(input("Introduce el primer número: "))
+    numero2=int(input("Introduce el segundo número: "))
+    if(numero2==0):
+        print("No es posible dividir entre 0")
+    else:
+        resultado1=(numero1 % numero2)
+        print(f"Resto de {numero1} % {numero2} = {resResto(numero1, numero2)}")
+
+#Exponenciación
+elif(opcion==7):
+    resExponente= lambda x, y: pow(x, y)
+    numero1=int(input("Introduce el primer número: "))
+    numero2=int(input("Introduce el exponente: "))
+    print(f"{numero1} elevado a {numero2} = {resExponente(numero1, numero2)}")
+
 #Área Cuadrado
 elif(opcion==8):
     resAreaCuadrado= lambda x: pow(x, 2)
@@ -80,3 +109,8 @@ elif(opcion==11):
     resAreaCirculo= lambda x: math.pi*x**2
     radio=int(input("Introduce el radio del triángulo: "))
     print(f"El área de un círculo de radio {radio} es {round(resAreaCirculo(radio), 2)} metros cuadrados")
+    
+#Salir
+elif(opcion==12):
+    print("Cerrando la calculadora...")
+    
